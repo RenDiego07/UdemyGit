34e770a54b4a3a2558b84dc3cf777e25ab2d916d
DiegoZz <dfflores@espol.edu.ec>
2023-03-23 22:52:28 -0500

    first commit

---
+++ src/flujosDeControl/objetosString.java
@@ -0,0 +1,17 @@
+package flujosDeControl;
+
+public class objetosString {
+    public static void main(String [] args) {
+            Integer [] caja= {1,2,3,4,5};
+            int lenghtIterando = caja.length;
+
+            Buscando_Numeros: for(int x =0 ; x< lenghtIterando; x++){
+                if(caja[x] <4)
+                    continue Buscando_Numeros;
+                System.out.println("si funciono");
+            }
+    }
+
+
+
+}
